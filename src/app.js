641973ed98a17ae91a4c32fb300d14b8f4edb278
Carlos Melchor Perez <carloos0665@gmail.com>
2025-03-03 04:50:08 +0000

    Add Styles

--- src/app.js
+++ src/app.js
@@ -7,6 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function () {
 
+  let contain = document.querySelector("#contain")
+
+  let divs = `  
+  <div id="cardGenerate">
+  <div id="symbolTop"></div>
+  <div id="value"></div>
+  <div id="symbolBottom"></div>
+  </div>`
+  contain.innerHTML = divs
+
+  let cardGenerate = document.querySelector("#cardGenerate")
+  cardGenerate.classList.add("bg-white", "w-25")
+
+  let value = document.querySelector("#value")
+  value.classList.add("d-flex", "justify-content-center")
+
+  let symbolTop = document.querySelector("#symbolTop")
+  symbolTop.classList.add("d-flex", "justify-content-start")
+
+
+  let symbolBottom = document.querySelector("#symbolBottom")
+  symbolBottom.classList.add("d-flex", "align-self-end")
+
+
+
+
   let symbols = ["♦", "♥", "♠", "♣"]
   let numCard = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   function RandoElements(arr) {
@@ -16,13 +42,12 @@ window.onload = function () {
   let valueElement = document.querySelector("#value")
   valueElement.innerText = RandoElements(numCard)
 
-  let symbol = RandoElements (symbols)
+  let symbol = RandoElements(symbols)
 
   let symbolElement1 = document.querySelector("#symbolTop")
   symbolElement1.innerText = symbol
 
   let symbolElement2 = document.querySelector("#symbolBottom")
-  symbolElement2.innerText = symbol  
-
+  symbolElement2.innerText = symbol
   return random
 };
