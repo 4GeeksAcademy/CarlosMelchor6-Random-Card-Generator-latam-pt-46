7b81ad4c13dca2eeff8998137856a1a24a121877
Carlos Melchor Perez <carloos0665@gmail.com>
2025-03-05 00:11:31 +0000

    card generate finish

--- src/app.js
+++ src/app.js
@@ -18,36 +18,45 @@ window.onload = function () {
   contain.innerHTML = divs
 
   let cardGenerate = document.querySelector("#cardGenerate")
-  cardGenerate.classList.add("bg-white", "w-25")
-
-  let value = document.querySelector("#value")
-  value.classList.add("d-flex", "justify-content-center")
+  cardGenerate.style.width = "25%"
+  cardGenerate.style.height = "75%"
+  cardGenerate.classList.add("mx-auto", "bg-white", "rounded", "mt-5")
 
   let symbolTop = document.querySelector("#symbolTop")
-  symbolTop.classList.add("d-flex", "justify-content-start")
+  symbolTop.style.fontSize = "120px"
+  symbolTop.style.marginTop = "-2.5rem"
+  symbolTop.style.marginLeft = "1rem"
 
+  let value = document.querySelector("#value")
+  value.style.fontSize = "180px"
+  value.classList.add("d-flex", "justify-content-center", "align-items-center")
 
   let symbolBottom = document.querySelector("#symbolBottom")
-  symbolBottom.classList.add("d-flex", "align-self-end")
-
-
-
+  symbolBottom.style.fontSize = "120px"
+  symbolBottom.style.marginRight = "1rem"
+  
 
   let symbols = ["♦", "♥", "♠", "♣"]
   let numCard = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
   function RandoElements(arr) {
     return arr[Math.floor(Math.random() * arr.length)]
   }
-  let random = `${RandoElements(symbols)} ${RandoElements(numCard)}`
-  let valueElement = document.querySelector("#value")
-  valueElement.innerText = RandoElements(numCard)
 
   let symbol = RandoElements(symbols)
+  let valueCard = RandoElements(numCard)
 
   let symbolElement1 = document.querySelector("#symbolTop")
   symbolElement1.innerText = symbol
 
+  let valueElement = document.querySelector("#value")
+  valueElement.innerText = valueCard
+
   let symbolElement2 = document.querySelector("#symbolBottom")
   symbolElement2.innerText = symbol
-  return random
+
+  if (symbol === "♦" || symbol === "♥") {
+    symbolElement1.style.color = "red"
+    symbolElement2.style.color = "red"
+  }
 };
